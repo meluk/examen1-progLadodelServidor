af7c04a4e327f281f13660b7647ea82634989eb2
meluk <kmelisu@hotmail.com>
2016-10-31 00:05:02 -0600

    examen1
    examen1

---
+++ server.js
@@ -0,0 +1,54 @@
+//server.js
+
+//establecer
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var nodemailer = require('nodemailer');
+var fs = require('fs');
+
+   
+
+ 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+
+var smtpTransport = nodemailer.createTransport('smtps://ladoservidor2016%40gmail.com:ladosrv2016@smtp.gmail.com');
+
+// routes ==============================================================
+ app.get('/send',function(req,res){
+
+    var mailOptions={
+        from: '"usuario" <ladoservidor2016@gmail.com>',
+        to : req.query.to,
+        subject : req.query.subject,
+        text : req.query.text
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});
+
+
+
+    // applicacion-------------------------------------------------------------
+    app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // carga la vista index 
+    });
+
+
+
+
+   
+
+app.listen(8080);
+console.log("App lista en el puerto 8080");
